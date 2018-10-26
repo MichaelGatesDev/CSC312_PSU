a8323601c54230b3e563d39e5589fe1577409afa
Michael Gates <michaelgatesdev@gmail.com>
2018-10-26 12:44:39 -0400

    Added comments

--- project1/src/csc312/grid/GridDirection.java
+++ project1/src/csc312/grid/GridDirection.java
@@ -2,6 +2,13 @@ package csc312.grid;
 
 public enum GridDirection
 {
+    /**
+     * TOP->BOTTOM or BOTTOM->TOP
+     */
     HORIZONTAL,
+    
+    /**
+     * LEFT->RIGHT or RIGHT->LEFT
+     */
     VERTICAL
 }
